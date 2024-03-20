852c07e0946c210875afbdc5105c117dfa15923d
Emily <emily.j.langley@gmail.com>
2024-03-20 09:12:25 -0700

    added main.js code

--- js-diplomacy/main.js
+++ js-diplomacy/main.js
@@ -11,6 +11,23 @@ function changeColor() {
 }
 
 // prevent default form behavior with a function
-function validateNumber() {
-    
+function validateNumber(event) {
+    // prevent default behavior until input checks below have run
+    event.preventDefault();
+
+    // check user number input and if a number and less than 10, allow submission
+    const numberInput = document.querySelector('#number-input');
+    const userNumber = Number(numberInput.value);
+
+    const formFeedback = document.querySelector('#formFeedback');
+    if (isNaN(userNumber) || userNumber >= 10) {
+        formFeedback.innerText = 'Please enter a smaller number.';
+    } else {
+        formFeedback.innerText = 'You are good to go!';
+    }
+
+// add event handlers
+document.querySelector('.color-changer').addEventListener('click', changeColor);
+document.querySelector('.number-form').addEventListener('submit', validateNumber);
+
 }
